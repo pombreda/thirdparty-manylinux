cdc5d4710da35545bcdb3224589b634da7271a4b
Philippe Ombredanne <pombredanne@nexb.com>
2016-10-28 13:15:10 +0200

    Build re2 and ensure we keep original sources
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -10,26 +10,35 @@ rm -rf /opt/python/cp33-*
 rm -rf /opt/python/cp34-*
 ls -al /opt/python/
 
-# Compile wheels
+# list of packages to build
+############################
+PACKAGES="intbitset bitarray lxml==3.6.4 \
+pyahocorasick \
+https://github.com/WojciechMula/pyahocorasick/archive/19282329183f465130cb9cb3538c9ac44c9cf796.zip \
+re2"
+
+
+# Compile (or fetch pre-built) wheels
+############################
 for PYBIN in /opt/python/*/bin; do
-    ${PYBIN}/pip wheel intbitset --wheel-dir=wheels
-    ${PYBIN}/pip wheel bitarray --wheel-dir=wheels
-    ${PYBIN}/pip wheel lxml==3.6.4 --wheel-dir=wheels
-    ${PYBIN}/pip wheel tinyarray --wheel-dir=wheels
-    ${PYBIN}/pip wheel pyahocorasick --wheel-dir=wheels
-    ${PYBIN}/pip wheel https://github.com/WojciechMula/pyahocorasick/archive/19282329183f465130cb9cb3538c9ac44c9cf796.zip --wheel-dir=wheels
+    for PACKAGE in $PACKAGES; do
+        ${PYBIN}/pip -v wheel --wheel-dir=wheels $PACKAGE
+    done
+done
+
+# Fetch sources
+############################
+PY35_BIN=/opt/python/cp35-cp35m/bin
+for PACKAGE in $PACKAGES; do
+    $PY35_BIN/pip -v download -d wheels --no-binary :all: $PACKAGE
 done
 
 ls -al wheels
+
+# Remove extra dep
 rm -rf wheels/six*whl
 
 # Bundle external shared libraries into the wheels
 #for whl in wheels/*.whl; do
 #    auditwheel repair $whl -w /io/wheels
 #done
-
-# Install packages and test
-# for PYBIN in /opt/python/*/bin/; do
-#     ${PYBIN}/pip install python-manylinux-demo --no-index -f /io/wheels
-#     (cd $HOME; ${PYBIN}/nosetests pymanylinuxdemo)
-# done
