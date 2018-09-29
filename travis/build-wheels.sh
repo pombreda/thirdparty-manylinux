287be49785189182a0a2b89a4ff94851c045825a
Philippe Ombredanne <pombredanne@nexb.com>
2018-09-29 11:22:54 +0200

    Do not attempt to build on 3.7
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,17 +4,23 @@ set -e -x
 # Install a system package required by our libraries
 yum install -y libxslt-devel python-devel libxml2 libxml2-devel libxslt
 
-
+# list all pythons
 ls -al /opt/python/
 
+# ... then delete the python we do not care for such that no build is done for these:
+# only keep 2.7 and 3.6
 rm -rf /opt/python/cp26-*
 rm -rf /opt/python/cp33-*
 rm -rf /opt/python/cp34-*
 rm -rf /opt/python/cp35-*
+# no 3.7 yet for now
+rm -rf /opt/python/cp37-*
 rm -rf /opt/_internal/cpython-2.6.*
 rm -rf /opt/_internal/cpython-3.3.*
 rm -rf /opt/_internal/cpython-3.4.*
 rm -rf /opt/_internal/cpython-3.5.*
+# no 3.7 yet for now
+rm -rf /opt/_internal/cpython-3.7.*
 
 ls -al /opt/python/
 
